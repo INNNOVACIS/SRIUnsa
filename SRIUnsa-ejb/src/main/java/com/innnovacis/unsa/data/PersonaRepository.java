b0e3ecb6e7363e737f6fb33c177e3d287c8037d2
innnovacis <ali.arapa@gmail.com>
2016-10-10 16:01:18 -0500

    start commit

--- SRIUnsa-ejb/src/main/java/com/innnovacis/unsa/data/PersonaRepository.java
+++ SRIUnsa-ejb/src/main/java/com/innnovacis/unsa/data/PersonaRepository.java
@@ -30,13 +30,7 @@ public class PersonaRepository {
 /**ACA ESTAS USANDO CRITERIOS DE HIBERNATE, USA TAMBIEN PROCEDMIENTO ALMACENADOS*/ 
 /*LECTURA DE UN PROCEDIMIENTO ALMACENADO*/
         
-//ali revisa pssssss jajaj
-// ILUSOOOOOOOOOOOOOOOOOOOOO
-// AAAAAAAAAAAAAAAAAAAAAAAAAA
-//DDDDDDDDDDDDDDDDDD
-// mañana gana Perú 20 a 0
 
-//que iluso
         Query query = em.createNativeQuery("{call getNombreById(1)}");
         List<String> list=query.getResultList();
         System.out.println("=====> " + list);
