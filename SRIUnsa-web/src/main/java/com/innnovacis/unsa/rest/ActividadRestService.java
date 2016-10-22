f2122470b285643a6e5c8e4417e6bbad8f396450
innnovacis <innnovacis@innnovacis>
2016-10-22 08:31:07 -0500

    Modificaciones Servicios Rest

--- SRIUnsa-web/src/main/java/com/innnovacis/unsa/rest/ActividadRestService.java
+++ SRIUnsa-web/src/main/java/com/innnovacis/unsa/rest/ActividadRestService.java
@@ -13,7 +13,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 import org.json.JSONObject;
 
 /**
@@ -29,7 +28,6 @@ public class ActividadRestService {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public String Filtro(String parametros) {
-        //Gson o = new Gson();
         
         HashMap<String, String> map = new HashMap<String, String>();
         JSONObject jObject = new JSONObject(parametros);
